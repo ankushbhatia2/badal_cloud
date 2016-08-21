8ffc87383cd3dbe48c963440cf497cf01281f281
Ankush Bhatia <coolankush07@gmail.com>
2016-08-21 23:23:50 +0530

    Added a license

--- badal.py
+++ badal.py
@@ -1,3 +1,29 @@
+""" Copyright 2016 Ankush Bhatia
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License. """
+
+#-------------------------------------------------------------------------------
+# Name:        badal.py
+# Purpose:     Badal the cloud hosts a cloud server to store photos and txt files
+#
+#
+#
+# Author:      Ankush Bhatia
+#
+# Created:     21/01/2016
+# Copyright:   Copyright 2016 Ankush Bhatia
+# Licence:     Apache License 2.0
+#-------------------------------------------------------------------------------
 # all the imports
 import time
 from sqlite3 import dbapi2 as sqlite3
